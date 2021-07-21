188e3b7056ce7468a3a14aef50090953de1a5f66
David <david.hastrich@gmail.com>
2021-07-21 19:26:13 +0200

    init

---
+++ ByteArrayExtensions.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace SeratoCrateWrapper
+{
+    public static class ByteArrayExtensions
+    {
+        /// <summary>
+        /// Searches for the specified pattern and returns the index of its first occurrence in a one-dimensional array or in a range of elements in the array.
+        /// </summary>
+        /// <param name="bytes">The one-dimensional array to search.</param>
+        /// <param name="pattern">The pattern to locate in "bytes".</param>
+        /// <param name="start">The starting index of the search.</param>
+        /// <returns>The index of the first occurrence of value, if it's found, within the range of elements in array that extends from startIndex to the last element; otherwise -1.</returns>
+        public static int IndexOf(this byte[] bytes, byte[] pattern, int start = 0)
+        {
+            if (bytes == null)
+                throw new ArgumentNullException(nameof(bytes));
+
+            if (pattern == null)
+                throw new ArgumentNullException(nameof(pattern));
+
+            if (start < 0)
+                throw new ArgumentOutOfRangeException(nameof(start), $"{nameof(start)} has to be positive.");
+
+            if (start > bytes.Length)
+                throw new ArgumentOutOfRangeException(nameof(start), $"{nameof(start)} has to be smaller than the length of bytes.");
+
+            if (pattern.Length > bytes.Length - start)
+                return -1;
+
+            var count = 0;
+            for (int i = start; i < bytes.Length - pattern.Length; i++)
+            {
+                bool found = true;
+                for (int j = 0; j < pattern.Length; j++)
+                {
+                    if (bytes[i + j] != pattern[j])
+                    {
+                        found = false;
+                        i += j;
+                        break;
+                    }
+                    count++;
+                }
+                if (found)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Returns a portion of the array from start to end index.
+        /// </summary>
+        /// <param name="bytes">The array.</param>
+        /// <param name="start">The start index<./param>
+        /// <param name="end">The end index.</param>
+        /// <returns>The poriton of the provided array.</returns>
+        public static byte[] Slice(this byte[] bytes, int start, int end)
+        {
+            var length = end - start;
+
+            if (bytes == null)
+                throw new ArgumentNullException(nameof(bytes));
+
+            if (start < 0)
+                throw new ArgumentOutOfRangeException(nameof(start), $"{nameof(start)} has to be positive.");
+
+            if (end < start)
+                throw new ArgumentOutOfRangeException(nameof(end), $"{nameof(end)} has to be greater than {nameof(start)}.");
+
+            if (start > bytes.Length)
+                throw new ArgumentOutOfRangeException(nameof(start), $"{nameof(start)} has to be smaller than the length of bytes.");
+
+            if (end > bytes.Length)
+                throw new ArgumentOutOfRangeException(nameof(end), $"{nameof(end)} has to be smaller than the length of bytes.");
+
+            var returnArray = new byte[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                returnArray[i] = bytes[i + start];
+            }
+
+            return returnArray;
+        }
+    }
+}
---
+++ SeratoCrate.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace SeratoCrateWrapper
+{
+    public class SeratoCrate
+    {
+        // V1.0 Support for reading/writing crate files with all Songs saved on the same Drive.
+
+        // ToDo V1.1 Support for songs from multiple drives in one crate.
+
+        // Columns writing (not yet supported)
+        //static readonly byte[] _osrt = Encoding.UTF8.GetBytes("osrt");
+        //static readonly byte[] _columnSortEnd = new byte[] { 0x00, 0x00, 0x00, 0x01, 0x00 };
+        //static readonly byte[] _tvcn = Encoding.UTF8.GetBytes("tvcn");
+        //static readonly byte[] _brev = Encoding.UTF8.GetBytes("brev");
+
+        //Crate identifier
+        static readonly byte[] _crateBeginning = Encoding.BigEndianUnicode.GetBytes("Serato ScratchLive");
+
+        // Columns reading
+        static readonly byte[] _columnBeginning = Encoding.UTF8.GetBytes("ovct");
+        static readonly byte[] _columnNameEnd = Encoding.UTF8.GetBytes("tvcw");
+
+        // Songs
+        static readonly byte[] _songBeginning = Encoding.UTF8.GetBytes("otrk");
+        static readonly byte[] _songFilePathEnd = Encoding.UTF8.GetBytes("ptrk");
+
+        public List<string> Songs;
+        private List<byte> _crateDataExcludingSongs;
+
+        private SeratoCrate(
+            List<string> songs,
+            List<byte> crateDataExcludingSongs) 
+        {
+            Songs = songs;
+            _crateDataExcludingSongs = crateDataExcludingSongs;
+        }
+
+        private static void ValidatePath(
+            string filePath)
+        {
+            var folderPath = Path.GetDirectoryName(filePath);
+
+            if (!Directory.Exists(folderPath))
+                throw new ArgumentOutOfRangeException(nameof(filePath), "Directory does not exist.");
+
+            if (Path.GetExtension(filePath) != ".crate")
+                throw new ArgumentOutOfRangeException(nameof(filePath), "File is not a .crate file");
+        }
+
+        /// <summary>
+        /// Loads an existing Serato ScratchLive Crate.
+        /// </summary>
+        /// <param name="filePath">The full path to the .crate file.</param>
+        /// <returns>An Instance of the SeratoCrate Class containing the song data read from the .crate file</returns>
+        public static SeratoCrate Load(
+            string filePath)
+        {
+            if (!File.Exists(filePath))
+                throw new ArgumentOutOfRangeException(nameof(filePath), "File does not exist.");
+
+            var fileBytes = File.ReadAllBytes(filePath);
+
+            if (fileBytes.IndexOf(_crateBeginning) == -1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(filePath), "File is not a serato crate.");
+            }
+
+            var songstart = fileBytes.IndexOf(_songFilePathEnd) + 8; // Advance to the start of the song info
+
+            return new SeratoCrate(
+                ReadSongs(fileBytes, songstart),
+                fileBytes.Slice(0, songstart - 16).ToList());
+        }
+
+        /// <summary>
+        /// Creates a new Instance of the SeratoCrate class with Serato's default settings
+        /// </summary>
+        /// <returns>A new Instance of the SeratoCrate class with Serato's default settings</returns>
+        public static SeratoCrate CreateNew()
+        {
+            return new SeratoCrate(
+                new List<string>(),
+                Properties.Resources.DefaultCrateWithoutSongs.ToList());
+        }
+
+        /// <summary>
+        /// Adds a song to the crate
+        /// </summary>
+        /// <param name="filePathWithoutDrivePart">The full path to the songfile, leaving out the drive identifier (C:\Music\test.mp3 -> Music\test.mp3) !!All songs have to be from the same drive as the crate!!</param>
+        public void AddSong(
+            string filePathWithoutDrivePart)
+        {
+            Songs.Add(filePathWithoutDrivePart);
+        }
+
+        /// <summary>
+        /// Removes a song from the crate
+        /// </summary>
+        /// <param name="filePathWithoutDrivePart">The full path to the songfile, leaving out the drive identifier (C:\Music\test.mp3 -> Music\test.mp3) !!All songs have to be from the same drive as the crate!!</param>
+        public void RemoveSong(
+            string filePathWithoutDrivePart)
+        {
+            if (!Songs.Contains(filePathWithoutDrivePart))
+                throw new ArgumentOutOfRangeException(nameof(filePathWithoutDrivePart), "This song is currently not in the crate.");
+            
+            Songs.Remove(filePathWithoutDrivePart);
+        }
+
+        /// <summary>
+        /// Saves the state of the Instance into a .crate file
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void WriteFile(
+            string filePath)
+        {
+            ValidatePath(filePath);
+
+            _crateDataExcludingSongs.AddRange(GetSongDataForWrite());
+
+            File.WriteAllBytes(filePath, _crateDataExcludingSongs.ToArray());
+        }
+
+        private static List<string> ReadSongs(byte[] crateFile, int pointer)
+        {
+            var songs = new List<string>();
+
+            var nextSongStart = crateFile.IndexOf(_songBeginning, pointer); // Get beginning of the next song
+            if (nextSongStart < 0)
+                nextSongStart = crateFile.Length;
+
+            while (true)
+            {
+                // Get path to songfile
+                var songData = crateFile.Slice(pointer, nextSongStart);
+                songs.Add(Encoding.BigEndianUnicode.GetString(songData));
+
+                // Advance to the beginning of the next song
+                pointer = crateFile.IndexOf(_songFilePathEnd, nextSongStart) + 8;
+                nextSongStart = crateFile.IndexOf(_songBeginning, pointer);
+
+                if (nextSongStart == -1)
+                    break;
+                 
+                nextSongStart = crateFile.IndexOf(_songBeginning, pointer);
+            }
+            return songs;
+        }
+
+        /// <summary>
+        /// Converts the songdata into the serato format
+        /// </summary>
+        /// <param name="songs">List of paths to the songfiles</param>
+        /// <returns>Bytes of the songdata.</returns>
+        private List<byte> GetSongDataForWrite()
+        {
+            var returnBytes = new List<byte>();
+
+            for (int i = 0; i < Songs.Count; i++)
+            {
+                var songFilePathBytes = Encoding.BigEndianUnicode.GetBytes(Songs[i]);
+
+                if (songFilePathBytes.Last() == 0) //Remove trailing byte
+                {
+                    songFilePathBytes = songFilePathBytes.Slice(0, songFilePathBytes.Length - 1);
+                }
+
+                var doubleLength = Songs[i].Length * 2;
+
+                //Conversion into the SSL Format
+                returnBytes.AddRange(_songBeginning); //Starttag for a song "otrk"
+                returnBytes.AddRange(BitConverter.GetBytes(doubleLength + 8).Reverse()); //Double length +8 of the songFile in BigEndianUnicode
+                returnBytes.AddRange(_songFilePathEnd); //"ptrk"
+                returnBytes.AddRange(BitConverter.GetBytes(doubleLength).Reverse()); //Double length of the songFile in BigEndianUnicode
+                returnBytes.AddRange(songFilePathBytes); //Path to the songFile in BigEndianUnicode
+            }
+
+            return returnBytes;
+        }
+    }
+}
